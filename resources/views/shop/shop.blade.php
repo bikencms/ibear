a4dc6d29a06d4da7b113ffd4dd3dfdf8d7a8f848
minhbikencms <minhbiken14@gmail.com>
2024-10-24 12:04:26 +0700

    optimize

--- resources/views/shop/shop.blade.php
+++ resources/views/shop/shop.blade.php
@@ -16,17 +16,16 @@
 
     <link rel="stylesheet" href="{{ url('assets/css/reset.css') }}">
 
-    <link rel="preload" href="{{ url('assets/css/bootstrap.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-    <noscript><link rel="stylesheet" href="{{ url('assets/css/bootstrap.css') }}"></noscript>
+    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.css') }}">
 
     <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+
     <link rel="stylesheet" href="vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
 
-    <link rel="preload" href="{{ url('vendors/simple-line-icons/css/simple-line-icons.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-    <noscript><link rel="stylesheet" href="{{ url('vendors/simple-line-icons/css/simple-line-icons.css') }}"></noscript>
+    <link rel="stylesheet" href="{{ url('vendors/simple-line-icons/css/simple-line-icons.css') }}">
 
-    <!-- <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css"  media="all"> -->
-    <!-- <link rel="stylesheet" href="{{ url('css/vertical-layout-light/style.css') }}"> -->
+    <!-- <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css"> -->
+    <link rel="stylesheet" href="{{ url('css/vertical-layout-light/style.css') }}">
     <link rel="stylesheet" href="{{ url('assets/css/shop.css') }}">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-TV45K6YQCE"></script>
